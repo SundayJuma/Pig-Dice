1136f62fa90a11903f6edcc8eacb1717741fe382
Sunday Juma <samanthasundayjuma@gmail.com>
2018-12-18 21:25:10 +0300

    add updated js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var roundScore = 0;
 
 
 $(document).ready(function() {
-  $("#btnRoll1").click(function() {
+  $("#btnRoll").click(function() {
 
 
     var finalScore = 50;
@@ -13,14 +13,26 @@ $(document).ready(function() {
       roundScore = 0;
       $("#btnRoll1").attr("disabled",true);
 
+      }else if (randomNumber === 1) {
+        $('#rolled2').text("You rolled 1. Next player's turn");
+        roundScore = 0;
+        $("#btnRoll2").attr("disabled",true);
+
 
     } else
     if (randomNumber !== 1) {
       roundScore += randomNumber;
 
-    } else if (roundScore > 50) {
+    } else if (roundScore >= 50) {
       $("#rolled1").text("Winner");
-    }
+    }else {
+
+    }$("#rolled2").text("Winner");
+    
     $('#totalPoints').text(roundScore);
   });
+
+  // $("#btnHold1").click(function(){
+  //   $('#score-1').text(roundScore);
+  // });
 });
