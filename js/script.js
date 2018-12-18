be476fccf057cd700ae4cedbdf597d5575ba5a0f
Sunday Juma <samanthasundayjuma@gmail.com>
2018-12-18 21:35:50 +0300

    add proper spacing and indentation

--- js/script.js
+++ js/script.js
@@ -1,35 +1,3 @@
-var roundScore = 0;
-
-
-$(document).ready(function() {
-  $("#btnRoll1").click(function() {
-
-
-    var finalScore = 50;
-    var randomNumber = Math.floor(Math.random() * 6) + 1;
-
-    if (randomNumber === 1) {
-      $('#rolled1').text("You rolled 1. Next player's turn");
-      roundScore = 0;
-      $("#btnRoll1").attr("disabled",true);
-
-
-    } else
-    if (randomNumber !== 1) {
-      roundScore += randomNumber;
-
-    } else if (roundScore > 50) {
-      $("#rolled1").text("Winner");
-    }
-    $('#totalPoints').text(roundScore);
-  });
-});
-
-
-  $("#btnHold1").click(function(){
-    $('#score-1').text(roundScore);
-  });
-
   var player1 = playerOne;
   var player2 = playerTwo;
 
@@ -44,7 +12,7 @@ $(document).ready(function() {
     this.totalPoints = 0;
     this.turn = turn;
 
-    // this.playername;
+  
   }
 
   /*Create If statement to check the current player
